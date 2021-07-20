4db6f30d7d3f5a4102c622cc39765784d63e0423
vaibhavchandra13 <82270230+vaibhavchandra13@users.noreply.github.com>
2021-07-20 16:53:15 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -10,7 +10,7 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = ""
+    API_KEY = "1880480487:AAEl94bnq1sFPVWnEc3JzjzGP-ChInzuUdM"
     OWNER_ID = "1445251630" # If you dont know, run the bot and do /id in your private chat with it
     OWNER_USERNAME = "WolfIzAlive"
 
